d067792bb32b0b715470d293ed7a19437a864502
jianger <wsyglsp23@gmail.com>
2024-03-03 15:54:10 +0800

    add log class

---
+++ Logger.cc
@@ -0,0 +1,38 @@
+#pragma once
+#include "Logger.h"
+#include <iostream>
+using namespace muduo;
+void Logger::setLogLevel(int LogLevel)
+{
+    Loglevel_ = LogLevel;
+}
+
+void Logger::log(std::string msg)
+{
+    switch (Loglevel_)
+    {
+    case INFO:
+        std::cout << "[INFO]:";
+        break;
+    case ERR:
+        std::cout << "[ERR]:" ;
+        break;
+    case FATAL:
+        std::cout << "[FATAL]:";
+        break;
+    case DBG:
+        std::cout << "[DBG]:";
+        break;
+    default:
+        break;
+    }
+    std::cout << "TIME: " << msg << std::endl;
+}
+
+Logger& Logger::getInstance()
+{
+
+    static Logger logger;
+    return logger;
+
+}
---
+++ Logger.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <string>
+
+#include "nocpy.h"
+/*log use format
+    LOG_INFO("%s%d",arg1,arg2)
+*/
+#define LOG_INFO(LogmsgFormat,...)\
+    do                  \
+    { \
+        Logger& logger = Logger::getInstance();\
+        logger.setLogLevel(INFO);\
+        char buf[1024] = {0};\
+        snprintf(buf,1024,logformat,##__VA_ARGS__);\
+        logger.log(buf);\             
+    } while (0)
+#define LOG_ERR(logformat,...)\
+    do                  \
+    { \
+        Logger& logger = Logger::getInstance();\
+        logger.setLogLevel(ERR);\
+        char buf[1024] = {0};\
+        snprintf(buf,1024,logformat,##__VA_ARGS__);\
+        logger.log(buf);\             
+    } while (0)
+#define LOG_FATAL(logformat,...)\
+    do                  \
+    { \
+        Logger& logger = Logger::getInstance();\
+        logger.setLogLevel(FATAL);\
+        char buf[1024] = {0};\
+        snprintf(buf,1024,logformat,##__VA_ARGS__);\
+        logger.log(buf);\             
+    } while (0)
+
+#ifdef MODULEDBG
+#define LOG_DBG(logformat,...)\
+    do                  \
+    { \
+        Logger& logger = Logger::getInstance();\
+        logger.setLogLevel(DBG);\
+        char buf[1024] = {0};\
+        snprintf(buf,1024,logformat,##__VA_ARGS__);\
+        logger.log(buf);\             
+    } while (0)
+#endif // !MODULEDBG
+
+enum Loglevel
+{
+    INFO,
+    ERR,
+    FATAL,
+    DBG
+};
+
+class Logger : nocpy
+{
+public:
+    void setLogLevel(int LogLever);
+    void log(std::string msg);
+    static Logger &getInstance();
+
+private:
+    Logger() = default;
+    int Loglevel_;
+};
